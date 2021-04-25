74ee5377584f11c2ca90734be31dc37de3c51264
石田佳介 <datenokei@gmail.com>
2021-04-25 15:04:20 +0900

    add

--- src/views/Searchresult.vue
+++ src/views/Searchresult.vue
@@ -20,7 +20,7 @@ export default {
         Search
     },
     created(){
-        axios.get(`http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=b65e720bb6d57313&large_area=Z011&text=${this.text}`)
+        axios.get(`/v1/?key=b65e720bb6d57313&keyword&text=${this.text}`)
         .then((response) => console.log(response));
     }
     // async created(){
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     devServer:{
         port:8080,
-        proxy: "http://webservice.recruit.co.jp/hotpepper/gourmet"
+        proxy: "http://webservice.recruit.co.jp/hotpepper/shop/v1/"
     }
 };
