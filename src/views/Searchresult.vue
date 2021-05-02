414e4f297fc30ad90f278b959e4e463b47e428ae
石田佳介 <datenokei@gmail.com>
2021-05-02 11:59:49 +0900

    add

--- src/views/Searchresult.vue
+++ src/views/Searchresult.vue
@@ -2,8 +2,12 @@
     <div class = "wrapper">
         <Header/>
         <h2>{{text}}についての情報</h2>
-        <p v-for="(value,key) in info" :key="key"></p>
-        <p>{{value.access}}</p>
+        <p v-for="(value,key) in info" :key="key">
+           <th>{{value.access}}</th>
+           <td>{{value.address}}</td>
+           <td>{{value.capacity}}</td>
+        </sp>
+        
     </div>
 </template>
 
@@ -15,8 +19,7 @@ import axios from "axios"
 export default {
     data(){
         return{
-            access: [],
-            info:[]
+            info:"",
         }
     },
     name:"Search",
@@ -31,10 +34,8 @@ export default {
             axios.get(`/v1/?key=b65e720bb6d57313&large_area=Z011&format=json&name=${this.text}`)
             .then(
             response => {console.log(response.data.results.shop)
-            response.data.results.shop[0].access })
-            .catch(response=>console.log(response));
-    // const fooddata = item.data
-    // this.access = fooddata.access   
+                this.info = response.data.results.shop})
+                .catch(response=>console.log(response));
     }
 }
 </script>
